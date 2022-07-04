f4bf95c8b71c1d6c9fdfa4d5cf0eb307f93392d3
SerheyShtepa <103119911+SerheyShtepa@users.noreply.github.com>
2022-07-04 09:18:11 +0300

    Add files via upload

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -8,6 +8,7 @@ import dataclasses
 class Board:
     board = list()
 
+    @staticmethod
     def check_win(board):
         win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
         for each in win_coord:
